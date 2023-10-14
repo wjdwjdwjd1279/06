f6a2bbb1cf8fabf244e672a629c4ee0bbb9ef890
wjdwjdwjd1279 <alfzltm9721@naver.com>
2023-10-14 18:35:42 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,19 +3,17 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-	void print_star(void)
+	void func1(void)
 	{
-		int i;
-		for(i=0; i<0; i++)
-			printf("*");
+		int x;
+		printf("func1 x is at %p\n",&x);
 	}
 	
 int main(int argc, char *argv[]) {
-	int i;
+	int x;
 	
-	print_star();
-	print_star();
-	print_star();
+	printf("main x is at %p\n",&x);
+	func1();
 	
 	return 0;
 }
